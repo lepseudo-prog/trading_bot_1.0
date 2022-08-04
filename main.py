8a950f32aea9ce9549bce7a96520be4434bfc0a3
Mathieu <lapseudomathieu@gmail.com>
2022-08-04 18:49:22 -0400

    .

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import pandas as pd
-import pandas_ta as ta
-mydf = pd.DataFrame()
-mydf.ta.indicators()
+import api_keys
+import bybit
+import time
+client = bybit.bybit(test=False, api_key=api_keys.api_key, api_secret=api_keys.api_secret)
+print("Successful Login")
+
