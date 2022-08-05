740219d5299a00db4938a9712a26ae6ee3b8c79e
Mathieu <lapseudomathieu@gmail.com>
2022-08-04 20:46:03 -0400

    .

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import api_keys
-import bybit
 import time
-client = bybit.bybit(test=False, api_key=api_keys.api_key, api_secret=api_keys.api_secret)
-print("Successful Login")
+from pybit import inverse_perpetual
+import talib
+
+#session_auth = inverse_perpetual.HTTP(
+#    endpoint="https://api.bybit.com",
+#    api_key=api_keys.api_key,
+#    api_secret = api_keys.api_secret
+#)
+#print(session_auth.get_wallet_balance(coin="USDT"))
 
