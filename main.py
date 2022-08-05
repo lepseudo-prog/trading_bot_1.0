f1c365bbe80ca3c2e1b5fe79b5ee0f349392b73b
Mathieu <lapseudomathieu@gmail.com>
2022-08-04 21:38:02 -0400

    .

--- main.py
+++ main.py
@@ -2,11 +2,21 @@ import api_keys
 import time
 from pybit import inverse_perpetual
 import talib
-
-#session_auth = inverse_perpetual.HTTP(
-#    endpoint="https://api.bybit.com",
-#    api_key=api_keys.api_key,
-#    api_secret = api_keys.api_secret
-#)
-#print(session_auth.get_wallet_balance(coin="USDT"))
+import pandas as pd
+#authentification with bybit API
+session_auth = inverse_perpetual.HTTP(
+    endpoint="https://api.bybit.com",
+    api_key=api_keys.api_key,
+    api_secret = api_keys.api_secret
+)
+#Set the time to get only 30 last candles
+now = int(time.time())
+min_30 = now - 1800
+data = session_auth.query_kline(
+    symbol="BTCUSD",
+    interval="1",
+    from_time=min_30
+)
+df = pd.DataFrame(data['result'])
+#data fetched, need indicators now
 
